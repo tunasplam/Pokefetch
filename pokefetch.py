cc268fb11891bf9e8b994d54f0c9665ac5c5d8f8
tunasplam <tunasplam@gmail.com>
2023-12-17 14:48:22 -0800

    inital conversion work

--- pokefetch.py
+++ pokefetch.py
@@ -1,104 +1,151 @@
-from bs4 import BeautifulSoup
-import requests
-import sys
-import re
+"""
+Port to pokebase API first. Then fix the existing issues.
 
-######## Get Pokemon Name ########
-if len(sys.argv) < 2:
-	pokemon = input("Enter the name of the pokemon to look up: ").title()
+Accept:
+    Name or pokedex ID
 
-# Two Word Names Like Mime Jr.
-elif len(sys.argv) == 3:
-	pokemon = (sys.argv[1] + " " + sys.argv[2]).title()
+    - maybe if the spelling is close enough then it fuzzes to the correct choice?
 
-else:
-	pokemon = sys.argv[1].title()
+TODO the output should be some sort of templated output. With
+cool things like bash coloring and italicizing and whatnot.
 
-if pokemon == "Mr. Mime":
-	pokemon = "Mr.Mime" #serebii listed this way
+verbose template and shortened template for like gameboy color?
 
-elif pokemon == "Ho-Oh":
-	pokemon = "Ho-oh" #serebii listed this way
+"""
 
-######## Get Pokedex Entry Number  ########
-begin_url = "http://serebii.net/pokedex-xy/"
-request1 = requests.get(begin_url)
-html1 = request1.content
-soup1 = BeautifulSoup(html1, "html.parser")
-page_info1 = soup1.get_text()
-# Pokedex Entries Start At Index 5000
-pokemon_index = page_info1.find(pokemon, 5000)
+import pokebase as pb
+import argparse
 
-if pokemon_index == -1:
-	print("You didn't enter a valid pokemon!")
-	sys.exit()
+bc = {
+    'NC': '\033[0m',
 
-pokedex_number = page_info1[pokemon_index-4 : pokemon_index-1]
+    'C0': '\033[0;30m',
+    'C1': '\033[0;31m',
+    'C2': '\033[0;32m',
+    'C3': '\033[0;33m',
+    'C4': '\033[0;34m',
+    'C5': '\033[0;35m',
+    'C6': '\033[0;36m',
+    'C7': '\033[0;37m',
 
-######## Get Pokemon Info ########
-full_url = begin_url + pokedex_number + ".shtml"
-request2 = requests.get(full_url)
-html2 = request2.content
-soup2 = BeautifulSoup(html2, "html.parser")
-page_info2 = soup2.get_text()
+    'BC0': '\033[1;30m',
+    'BC1': '\033[1;31m',
+    'BC2': '\033[1;32m',
+    'BC3': '\033[1;33m',
+    'BC4': '\033[1;34m',
+    'BC5': '\033[1;35m',
+    'BC6': '\033[1;36m',
+    'BC7': '\033[1;37m',
 
-######## Get Stats ########
-base_total = soup2.find(string=re.compile("Base Stats - Total:"))
-# Only want everything after Total
-base_total = base_total[base_total.find("Total"):]
-base_stats = soup2.find_all(class_="fooinfo")
+    'UC0': '\033[4;30m',
+    'UC1': '\033[4;31m',
+    'UC2': '\033[4;32m',
+    'UC3': '\033[4;33m',
+    'UC4': '\033[4;34m',
+    'UC5': '\033[4;35m',
+    'UC6': '\033[4;36m',
+    'UC7': '\033[4;37m'
+}
 
-hp = str(base_stats[len(base_stats)-21])
-attack = str(base_stats[len(base_stats)-20])
-defence = str(base_stats[len(base_stats)-19])
-spec_attack = str(base_stats[len(base_stats)-18])
-spec_defence = str(base_stats[len(base_stats)-17])
-speed = str(base_stats[len(base_stats)-16])
+def main():
+    parser = argparse.ArgumentParser(
+        prog='PokeFetch',
+        description='Displays cool pokemon sprites/stats in terminal.',
+        epilog='Have fun.'
+    )
 
-hp = hp[hp.find(">")+1 : hp.find("/")-1]
-attack = attack[attack.find(">")+1 : attack.find("/")-1]
-defence = defence[defence.find(">")+1 : defence.find("/")-1]
-spec_attack = spec_attack[spec_attack.find(">")+1 : spec_attack.find("/")-1]
-spec_defence = spec_defence[spec_defence.find(">")+1 : spec_defence.find("/")-1]
-speed = speed[speed.find(">")+1:speed.find("/")-1]
+    # accepts either pokedex number (int) or name (str)
+    parser.add_argument('pokemon', help='Select a pokemon by name or pokedex ID.')
 
-######## Get Abilities ########
-abilities_index = page_info2.find("Abilities:")
-abilities = page_info2[abilities_index:]
-end_index = abilities.find("\n")
-abilities = abilities[:end_index-1]
+    args = parser.parse_args()
+    
+    handle_pokemon(args.pokemon)
 
-######## Get Gender ########
-gender_index = page_info2.find("GenderType")
-gender = page_info2[gender_index:]
-end_index = gender.find("\n")
-gender = gender[:end_index]
-gender = gender[gender.find(" ")+1:]
-if "is Genderless" in gender:
-	gender = "Genderless"
+def handle_pokemon(p):
 
-######## Get Image ########
-base_image_url = "http://www.serebii.net/xy/pokemon/"
-full_image_url = base_image_url + pokedex_number + ".png"
-image_bytes = requests.get(full_image_url)
+    # lookup the pokemon    
+    poke = lookup_pokemon(p)
+    poke_extra = lookup_pokemon_species(p)
 
-# Write bytes of image to file
-filename = pokemon.title() + ".png"
-myfile = open("imgs/"+filename, "wb")
-for byte in image_bytes:
-    myfile.write(byte)
-myfile.close()
 
-######## Print everything out ########
-print("Name: ", pokemon)
-print("Pokedex Number: ", pokedex_number)
-print("Gender Ratio: ", str(gender))
-print(abilities)
-print("---Base Stats---")
-print(base_total)
-print("Hp: ", hp)
-print("Attack: ", attack)
-print("Defence: ", defence)
-print("Special Attack: ", spec_attack)
-print("Special Defence: ", spec_defence)
-print("Speed: ", speed)
+    with open('template.txt', 'r') as f:
+        output = f.read()
+    
+    print(poke.stats)
+
+    output = output.format_map({
+        'name':         poke.name.title(),
+        'ID':           poke.id_, 
+        'gender_info':  format_gender(poke_extra.gender_rate),
+        'abilities':    format_abilities(poke.abilities),
+    } | bc | stats_dict(poke))
+
+    print(output)
+
+def lookup_pokemon(p):
+    if p.isdigit():
+        return pb.pokemon(int(p))
+    else:
+        return pb.pokemon(p)
+    
+def lookup_pokemon_species(p):
+    # extra stats such as gender info are found in here
+    if p.isdigit():
+        return pb.pokemon_species(int(p))
+    else:
+        return pb.pokemon_species(p)
+
+def format_gender(e):
+    # takes the gender rate for female (in 8ths)
+    # and returns the formatted line for gender info.
+    # A value of -1 means genderless
+    gender_info = ''
+    if e == -1:
+        gender_info = 'genderless'
+    else:
+        # TODO male/female symbols.
+        f_pct = round(100*(e/8.0))
+        m_pct = 100-f_pct
+        gender_info = f'F: {f_pct}% M: {m_pct}%'
+
+    return gender_info
+
+def format_abilities(a):
+    result = ''
+    for ability in a:
+        result += f'\t- {ability.ability.name}'
+
+        if ability.is_hidden:
+            result += ' [HIDDEN]\n'
+        else:
+            result += '\n'
+    return result
+
+def stats_dict(poke):
+    # creates a dict of the stats for formatting the template
+    res = {}
+    for stat in poke.stats:
+        res[stat.stat.name] = stat.base_stat
+    
+    return res
+
+# # Print all moves of the type named here.
+# TYPE = 'normal'
+
+# # Good method.
+# # Get API data associated with the type we want.
+# type_moves = pb.type_(TYPE).moves
+
+# # Iterate & print.
+# for move in type_moves:
+#     print(move.name)
+
+# POKEMON = 'ditto'
+
+# poke = pb.pokemon(POKEMON)
+
+# for ability in poke.abilities:
+#     print(ability.is_hidden)
+#     print(ability.ability.name)
+
+main()
